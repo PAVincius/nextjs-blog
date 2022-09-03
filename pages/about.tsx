2512a89fdbc3b80d45d02ce3aa71ae8e47a744d1
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-09-02 23:00:13 -0300

    :sparkles: feat: about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,9 @@
+import { Article } from "@components/Article"
+
 export default function About () {
-    return {
-        
-    }
+    return (
+        <Article>
+            <h1>about page</h1>
+        </Article>
+    )
 }
